7144979cce14cd6346b2db206d75d7ff1a0dc41c
Ursula Moff <umoff@il-i4.21-school.ru>
2020-01-30 17:20:54 +0300

    1

--- includes/color.h
+++ includes/color.h
@@ -6,7 +6,7 @@
 /*   By: umoff <umoff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 12:54:25 by umoff             #+#    #+#             */
-/*   Updated: 2020/01/30 16:39:15 by umoff            ###   ########.fr       */
+/*   Updated: 2020/01/30 17:20:44 by umoff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@
 # define MENU_BACKGROUND	0x1E1E1E
 
 # define COLOR_LILAC		0x7B68EE
-# define COLOR_BLUESKY	0x00BFFF //BLUESKY
-# define COLOR_LIME		0x00FF00 //LIME
-# define COLOR_GOLD		0xFFD700 //GOLD
-# define COLOR_RASPBERRIES		0xFF0000 //RASPBERRIES
+# define COLOR_BLUESKY	    0x00BFFF
+# define COLOR_LIME		    0x00FF00
+# define COLOR_GOLD		    0xFFD700
+# define COLOR_RASPBERRIES  0xFF0000
 
 #endif
